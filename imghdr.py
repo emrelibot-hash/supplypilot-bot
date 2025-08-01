311b85c27a33cdf62da96d52abf8403898958044
emrelibot-hash <emreli.bot@gmail.com>
2025-08-02 00:38:05 +0400

    Create imghdr.py

---
+++ imghdr.py
@@ -0,0 +1,8 @@
+# imghdr.py — stub для python-telegram-bot, чтобы import imghdr не падал
+# https://docs.python.org/3/library/imghdr.html
+
+def what(file, h=None):
+    """
+    Всегда возвращает None — мы не используем детекцию формата картинки на этом этапе.
+    """
+    return None
