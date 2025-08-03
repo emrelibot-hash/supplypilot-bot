ad1aad3c6b6f6a99b190be2ed243380b81ffa80a
emrelibot-hash <emreli.bot@gmail.com>
2025-08-03 20:36:27 +0400

    Update and rename imghdr.py to .env.example

--- imghdr.py
+++
@@ -1,8 +0,0 @@
-# imghdr.py — stub для python-telegram-bot, чтобы import imghdr не падал
-# https://docs.python.org/3/library/imghdr.html
-
-def what(file, h=None):
-    """
-    Всегда возвращает None — мы не используем детекцию формата картинки на этом этапе.
-    """
-    return None
