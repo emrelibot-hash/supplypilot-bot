17c547f992309551d8ee15a5e500f9990b7796c3
emrelibot-hash <emreli.bot@gmail.com>
2025-08-15 22:25:11 +0400

    Update utils.py
    added

--- utils.py
+++ utils.py
@@ -12,4 +12,3 @@ def extract_excel_from_bytes(file_bytes, filename=None):
         return pd.read_excel(io.BytesIO(file_bytes), engine='xlrd')
     else:
         return pd.read_excel(io.BytesIO(file_bytes), engine='openpyxl')
-
